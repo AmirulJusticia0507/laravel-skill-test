af1f80d448feca4b5352a5db6a56d0fb581d57be
amirulputra0507 <amirulputra0507@gmail.com>
2026-01-27 23:02:39 +0700

    Register PostPolicy in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,24 +8,12 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
-        // daftar model => policy
         Post::class => PostPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
-
-        // Jika mau bisa tambahkan Gate custom di sini
-        // Gate::define('some-action', fn(User $user) => ...);
     }
 }
